b48c0f3a8ed0ee36276fd6e27ae618d5e04513ed
nikoada <nikushac@gmail.com>
2021-10-11 21:39:53 +0200

    No BS TS #16 - Abstract Classes in Typescript

---
+++ abstract.ts
@@ -0,0 +1,37 @@
+abstract class StreetFighter {
+    constructor() {}
+
+    move() {}
+    fight() {
+        console.log(`attack with ${this.getSpecialAttack()}`)
+    }
+
+    abstract getSpecialAttack(): string;
+    abstract get name(): string;
+}
+
+class Ryu extends StreetFighter {
+    getSpecialAttack(): string {
+        return "Hadoken";
+    }
+
+    get name(): string {
+        return "Ryu";
+    }
+}
+
+class ChunLi extends StreetFighter {
+    getSpecialAttack(): string {
+        return "Lightning Kick";
+    }
+
+    get name(): string {
+        return "Chun-Li";
+    }
+}
+
+const ryu = new Ryu();
+const chunLi = new ChunLi();
+
+ryu.fight();
+chunLi.fight();
